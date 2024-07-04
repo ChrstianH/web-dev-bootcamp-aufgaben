5ce79da013e3012435b752a7d53a84f2e55f6cfe
hensc <henschel_christian@web.de>
2024-07-04 14:48:49 +0200

    tag 16 solutions

---
+++ tag_16_aufgaben/String-Methods-Level-1_11/assets/js/main.js
@@ -0,0 +1,17 @@
+let x = "Hello";
+let y = "World";
+
+let z = x + y;
+document.write(z);
+document.write("<br>");
+
+let n = x + " " + y;
+document.write(n);
+document.write("<br>");
+
+x += " World";
+document.write(x);
+
+let meinString = "Ich bin Erste:r";
+meinString += " - Ich komme auf Platz zwei.";
+console.log(meinString);
---
+++ tag_16_aufgaben/Variablen-Level-1_6/assets/js/main.js
@@ -0,0 +1,10 @@
+/*
+let x = 34;
+x = 67;
+console.log(x);
+*/
+
+const y = 34;
+y = 67;
+console.log(y);
+// Fehlermeldung in Zeile 8, weil versucht wurde, eine const-Variable zu überschreiben
